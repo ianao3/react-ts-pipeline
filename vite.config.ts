b03c9d2fb5b666246d9b9d32031908a22f1aad33
ianao3 <58729233+ianao3@users.noreply.github.com>
2023-08-08 16:04:47 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import svgrPlugin from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react'
+import ViteVisualizer from "rollup-plugin-visualizer";
+
+// https://stackoverflow.com/a/15802301
+const headCommitHash = require('child_process')
+    .execSync('git rev-parse HEAD')
+    .toString();
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    __HEAD_COMMIT_HASH__: JSON.stringify(headCommitHash),
+  },
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: './build',
+  },
+  base: './',
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+    // last one is the visualizer
+    ViteVisualizer({
+      filename: './build/report-rollup-plugin-visualizer.html',
+      brotliSize: true,
+    }),
+  ],
+})
